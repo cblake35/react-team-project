d80f970c6085d9d528312c7916bd1cd57e1c2aab
Chris Blake <c.blake@Chriss-Air.attlocal.net>
2021-07-03 15:24:21 -0400

    Added initial GitHub app functionality

--- src/components/GitHubCommits.js
+++ src/components/GitHubCommits.js
@@ -1,10 +1,37 @@
-import React, {useEffects, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 
 const GitHubCommits = (props) => {
-    return(
+    const [name, setName] = useState('');
+    const [date, setDate] = useState('');
+    const [message, setMessage] = useState('');
+
+    const displayData = (commitData) => {
+        for (let i = 0; i < commitData.length; i++) {
+            console.log(commitData[i].commit.author.name)
+            console.log(commitData[i].commit.author.date)
+            console.log(commitData[i].commit.message)
+        }
+    }
+
+    useEffect(() => {
+        fetch('https://api.github.com/repos/cblake35/react-team-project/commits')
+            .then((res) => res.json())
+            .then((data) => displayData(data))
+    }, [])
+
+
+
+    return (
         <div className='mainDiv'>
             <div className='componentBody'>
-                <p>GitHub Commits</p>
+                <h1 className='commitHeading'>Commits</h1>
+                <div>
+                    <h3>Name</h3>
+                    <div>
+                        <p>Message</p>
+                        <p>Date</p>
+                    </div>
+                </div>
             </div>
         </div>
     )
