599ad6514c539141f0122086085fd3b84f0da2c9
Chris Blake <c.blake@Chriss-Air.attlocal.net>
2021-07-04 01:09:23 -0400

    Added styling for GitHub cards and responsiveness

--- src/components/GitHubCommits.js
+++ src/components/GitHubCommits.js
@@ -1,4 +1,9 @@
 import React, { useEffect, useState} from 'react';
+import {
+    Container,
+    Row,
+    Col 
+} from 'reactstrap';
 
 const GitHubCommits = () => {
     const [results, setResults] = useState([]);
@@ -13,18 +18,18 @@ const GitHubCommits = () => {
         <div className='mainDiv'>
             <div className='componentBody'>
                 <h1 className='commitHeading'>Commits</h1>
-                <div className='commitWrapper'>
+                <Row className='commitWrapper'>
                     {console.log(results),
                         results.map(result => {
                             return (
-                                <div className='commitCard'>
+                                <Col xs='6' md='4' lg='2' className='commitCard'>
                                     <h3>{result.commit.author.name}</h3>
                                     <p className='commitMsg'>{result.commit.message}</p>
                                     <p className='commitDate'>{result.commit.author.date}</p>
-                                </div>
+                                </Col>
                             )
                         })}
-                </div>
+                </Row>
             </div>
         </div>
     )
