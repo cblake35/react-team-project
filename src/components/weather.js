7159159b4cb45573c0491eaa8339bdf8e4e49334
Brad Titus <bltitus@me.com>
2021-07-04 00:48:54 -0400

    work on fetch issues in weather

--- src/components/weather.js
+++ src/components/weather.js
@@ -6,28 +6,31 @@ const key = '1fda8e8934336996f687fcbc3a8d70a1';
 const Weather = (props) => {
     const lat =  props.lat;
     const lon = props.lon;
-    const [ results, setResults ] = useState({});
-
-    let url = `${baseURL}lat=${lat}&lon=${lon}&appid=${key}`;
-    console.log(url);
+    const [ results, setResults ] = useState([]);
 
     useEffect(() => {
+        let url = `${baseURL}lat=${lat}&lon=${lon}&units=imperial&appid=${key}`;
+        console.log(url);
         fetch(url)
         .then((res) => res.json())
-        .then((data) => console.log(data))
+        .then((data) => setResults(data))
         .catch((err) => console.log(err))
-    });
+    }, []);
     
-    // console.log(results);
+    console.log(results);
+
 
     return (
         <div>
             <p>
-                {props.lat}
+                You are in {results.name}.
+            </p>
+            {/* <p>
+                It is currently {results.main.temp} degrees, and feels like {results.main.feels_like}.
             </p>
             <p>
-                {props.lon}
-            </p>
+                The weather is {results.weather.main}. 
+            </p> */}
         </div>
     )
 }
