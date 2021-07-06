d86716fb6ede58a1787a995f2ad8cac3311bcf6a
unknown <arianne.domka@gmail.com>
2021-07-06 13:25:03 -0400

    Fixed url misfire, img displayed correclty utilizing user location

--- src/components/NASA.js
+++ src/components/NASA.js
@@ -1,68 +1,28 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import App from '../../../react-team-project/src/App';
-import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
+import { Card, CardText, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
 
 const baseURL = 'https://api.nasa.gov/planetary/earth/imagery';
 const key = 'wklLsmFXRdX9oznv1ySgHE6LXELebXGRSlvj5qul';
 
 const NASA = (props) =>{
-    // const lat =  props.lat;
-    // const lon = props.lon;
-    // const [ results, setResults ] = useState([]);
-    // const [ search, setSearch ] = useState('');
-    // const [ date, setDate ] = useState('');
-    // const [ position, setPosition] = useState('');
-    
+
     let info = JSON.parse(window.localStorage.getItem('coordinates'));
     let lat = info[0][0];
     let lon = info[0][1];
-
-    // useEffect(() => {
-    // const fetchResults = () => {        
-        let url = `${baseURL}?lon=${lon}&lat=${lat}&dim=0.15&api-key=${key}`;
-        // url = date ? url + `&current_date=${date}` : url;
+    let url = `${baseURL}?lon=${lon}&lat=${lat}&dim=0.15&api_key=${key}`;
         console.log(url);
-//     fetch(url)
-//     .then(res => res.json())
-//     .then(data => setResults(data))
-//     .catch(err => console.log(err));
-// }, [lat, lon]);
-
-// console.log(results)
-
-// const handleSubmit = (event) => {
-//     event.preventDefault();
-    // fetchResults();
-    // console.log(results)
-// }
-
     return(
         <div>
             <div className="main">
-            {/* {results ? 
-                <span> Today, here is a satellite image at your location at or near {results.name}.</span>
-                    : null } */}
-                    
-
-
-                    <Card>
-                        {/* <CardImg> */}
-                            
-                <img src={url} alt='card cap' />
-                {/* <img src='https://api.nasa.gov/planetary/earth/imagery?lon=-87.43&lat=41.52&dim=0.15&api_key=wklLsmFXRdX9oznv1ySgHE6LXELebXGRSlvj5qul
-' alt='card cap' />  */}
-<CardBody>
-                    <CardTitle>Regular Ole Function</CardTitle>
-                    <CardSubtitle>A JS Library</CardSubtitle>
-                    <CardText><pre>const HelloWorld = function()</pre></CardText>
+                <Card>
+                <img src={url} alt='Satellite pic' width="350px" height="350px"/> 
+                <CardBody>
+                    <CardTitle>NASA Satellite Image</CardTitle>
+                    <CardSubtitle>Based on current location</CardSubtitle>
+                    <CardText><dd>Most recent LANDSAT image for these coordinates</dd></CardText>
                 </CardBody>
-                        {/* </CardImg> */}
             </Card>
-
-
-
-
-
         </div>
         </div>
     );
